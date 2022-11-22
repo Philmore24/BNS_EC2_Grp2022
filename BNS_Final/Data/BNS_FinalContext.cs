74e36e45f4079fd77ad7006a0904eb1a55b863a0
Philmore Foster <fosterphilmore@outlook.com>
2022-11-22 13:42:41 -0500

    Add project files.

---
+++ BNS_Final/Data/BNS_FinalContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BNS_Final.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace BNS_Final.Data
+{
+    public class BNS_FinalContext : IdentityDbContext<ApplicationUser>
+    {
+        public BNS_FinalContext()
+        {
+
+        }
+        public BNS_FinalContext (DbContextOptions<BNS_FinalContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Account> Account { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Teller> Teller { get; set; }
+
+        public DbSet<Transaction> accountTransaction { get; set; }
+
+    }
+}
---
+++ BNS_Final/Models/Account.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BNS_Final.Models
+{
+    public class Account
+    {
+        [Key]
+        
+        public long accountNumber { get; set; }
+
+        [DataType(DataType.Currency)]
+        public float balance { get; set; }
+
+        public string cardNum { get; set; }
+
+        public float availableAmount { get; set; }
+
+        public string type { get; set; }
+
+        public string userID { get; set; }
+    }
+}
